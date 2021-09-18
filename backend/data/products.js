b7ffbd2f40dd75b3b30278d51a0aa090549ccb2d
wiem140 <wiemjaballah88@gmail.com>
2021-09-18 12:35:21 +0100

    first commit

---
+++ backend/data/products.js
@@ -0,0 +1,93 @@
+const products =[
+    {
+        name : "AleoVera",
+        imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSk_5GHW1Ms3ftphhuoMu_aE8Ge2oYTOIb0Dw&usqp=CAU",
+        description:"L'aloé vera est une plante d'intérieur décorative, facile d'entretien et peu exigeante tout au long de l'année. Elle es utilisée pour ses vertus médicinales. ",
+        price: 6,
+        countInStock:50,
+    },
+  /*  {
+        name : "Jasmin",
+        imageUrl: "",
+        description:"",
+        price: 10,
+        countInStock:20,
+    },
+
+    {
+        name : "Menthe",
+        imageUrl: "",
+        description:"",
+        price: 2,
+        countInStock:100,
+    },
+    {
+        name : "Romarin",
+        imageUrl: "",
+        description:"",
+        price: 7,
+        countInStock:30,
+    },
+    {
+        name : "Oiseaux de Paradis",
+        imageUrl: "",
+        description:"",
+        price: 30,
+        countInStock:50,
+    },
+    {
+        name : "Citronelle",
+        imageUrl: "",
+        description:"",
+        price: 9,
+        countInStock:35,
+    },
+    {
+        name : "Basilic",
+        imageUrl: "",
+        description:"",
+        price: 3,
+        countInStock:25,
+    },
+    {
+        name : "lavande",
+        imageUrl: "",
+        description:"",
+        price: 8,
+        countInStock:60,
+    },
+    {
+        name : "géranium",
+        imageUrl: "",
+        description:"",
+        price:10,
+        countInStock:40,
+    },
+    {
+        name : "Glaieul",
+        imageUrl: "",
+        description:"",
+        price:30,
+        countInStock:35,
+    },
+    {
+        name : "Jacente",
+        imageUrl: "",
+        description:"",
+        price: 15,
+        countInStock:25,
+    },
+    {
+        name : "Amerilyse",
+        imageUrl: "",
+        description:"",
+        price: 19,
+        countInStock:24,
+    },*/
+
+
+
+
+
+];
+module.exports= products;
