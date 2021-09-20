e73e6ac19150062d8d4a719651332df0ee36af46
wiem140 <wiemjaballah88@gmail.com>
2021-09-20 14:02:59 +0100

    first commit

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-const productRoutes = require("./routes/produxtRoutes");
+const productRoutes = require("./routes/productRoutes");
 const connectDB = require("./config/db");
 
 connectDB();
@@ -15,5 +15,5 @@ app.get("/", (req, res) => {
 
 app.use("/api/products", productRoutes);
 
-const PORT = process.env.PORT || 6000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
