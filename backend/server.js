5ab79eb8be4ad49ba41d413218a3bbd1346da1e9
wiem140 <wiemjaballah88@gmail.com>
2021-09-23 12:54:08 +0100

    make it better

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const productRoutes = require("./routes/productRoutes");
 const connectDB = require("./config/db");
-
+const path = require('path');
 connectDB();
 
 const app = express();
@@ -15,5 +15,13 @@ app.get("/", (req, res) => {
 
 app.use("/api/products", productRoutes);
 
+// serve static assets if in production
+if (process.env.NODE_ENV=== 'production'){
+  //set static folder
+  app.use (express.static('frontend/build'));
+  app.get('*',(req,res)=>{
+res.sendFile(path.resolve(__dirname, 'fronted','build','index.html'));
+  });
+}
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
